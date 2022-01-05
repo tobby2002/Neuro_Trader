83befb627aeeb71c378d141c80b3bb4301664053
tradyai <tobby2002@gmail.com>
2022-01-05 15:39:13 +0900

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -15,14 +15,30 @@ def check_has_same_wavepattern_df(df, wavepattern_up):
             return True
     return False
 
-dfw = pd.DataFrame([], columns=['id', 'symbol', 'wave', 'position'])
+def entry_wave_df(df, df_ohlcv=None):
+    r_df = None
+
+    return df
+
+dfw = pd.DataFrame([], columns=['id', 'symbol', 'time', 'low', 'w1', 'w2', 'w3', 'w4', 'high', 'wave', 'position'])
+
 
 
 
                             wavepatterns_up.add(wavepattern_up)
                             # if not check_has_same_wavepattern(w_l, wavepattern_up):
                             if not check_has_same_wavepattern_df(dfw, wavepattern_up):
-                                w_info = [id(wavepattern_up), symbol, wavepattern_up, False]
+
+                                w_info = [id(wavepattern_up),
+                                          symbol,
+                                          wavepattern_up.dates[0],
+                                          wavepattern_up.low,
+                                          wavepattern_up.values[1],
+                                          wavepattern_up.values[3],
+                                          wavepattern_up.values[5],
+                                          wavepattern_up.values[7],
+                                          wavepattern_up.high,
+                                          wavepattern_up, False]
                                 w_l.append(w_info)
                                 dfw = dfw.append(pd.DataFrame([w_info], columns=dfw.columns), ignore_index=True)
-                                print(f'{rule.name} found: {new_option_impulse.values}')
+        
