7146a81d0aa9498d5f1ca3449368584e8af8e05b
tradyai <tobby2002@gmail.com>
2022-02-08 03:06:45 +0900

    Add files via upload

--- runwave.py
+++ runwave.py
@@ -31,16 +31,22 @@ import json
 from tapy import Indicators
 import os
 
+try:
+    from datapostgre import DataManagerfeeder
+except:
+    from .datapostgre import DataManagerfeeder
+
 with open('config.json', 'r') as f:
     config = json.load(f)
 
-def check_has_same_wavepattern_df(df_wave, wavepattern_up):
+def check_has_same_wavepattern_df(df_wave, wavepattern):
     l = df_wave.wave.tolist()
     if l:
         for i in l:
-            eq_dates = np.array_equal(np.array(wavepattern_up.dates), np.array(i[1]))
-            eq_values = np.array_equal(np.array(wavepattern_up.values), np.array(i[2]))
-            if eq_dates and eq_values:
+            # eq_dates = np.array_equal(np.array(wavepattern.dates), np.array(i[1]))
+            eq_values = np.array_equal(np.array(wavepattern.values), np.array(i[2]))
+            # if eq_dates and eq_values:
+            if eq_values:
                 return True
     return False
 
@@ -80,6 +86,16 @@ def agent_filter_and_update_wave_df(df_wave, symbol,  bin_size, bin_time, nowpoi
         df_wave.loc[:, ('status')] = df_wave.apply(lambda x: get_status_and_candle_trajectory(
             df_candle, nowpoint, x['end_time'], x['low'], x['w4'], x['high'], x['position'], x['status']), axis=1)
     return df_wave
+
+
+def fractals_low_loopA(df, fractal_count=5, loop_count=1):
+    for c in range(loop_count):
+        window = 2 * fractal_count + 1
+        df['fractals_low'] = df['Low'].rolling(window, center=True).apply(lambda x: x[fractal_count] == min(x), raw=True)
+        df = df[~(df[['fractals_low']].isin([0, np.nan])).all(axis=1)]
+        df = df.dropna()
+        df = df.drop(['fractals_low'], axis=1)
+    return df
 
 
 def fractals_low_loop(df):
@@ -135,6 +151,8 @@ window = config['default']['window']
 round_trip_flg = config['default']['round_trip_flg']
 round_trip_count = config['default']['round_trip_count']
 compounding = config['default']['compounding']
+fractal_count = config['default']['fractal_count']
+loop_count = config['default']['loop_count']
 
 exchange = config['default']['exchange']
 timeframe = config['default']['timeframe']
@@ -154,10 +172,16 @@ plotview = config['default']['plotview']
 printout = config['default']['printout']
 symbols = config['symbols']['binance_futures']
 
+def check_has_same_wavepattern(w_l, wavepattern):
+    for i in w_l:
+        eq_dates = np.array_equal(np.array(wavepattern.dates), np.array(i[-1].dates))
+        eq_values = np.array_equal(np.array(wavepattern.values), np.array(i[-1].values))
+        if eq_dates and eq_values:
+            return True
+    return False
 
-def wave_analyzer(dm, exchange, symbols, bin_size, bin_time, up_to_count, startpoint, nowpoint, window, truncate=False):
-    pid = os.getpid()
-    # print('wave_analyzer start pid: %s' % pid)
+def wave_analyzer(exchange, symbols, bin_size, bin_time, up_to_count, startpoint, nowpoint, window, truncate=False):
+    dm = DataManagerfeeder()
     conn = dm.get_conn()
     if truncate:
         dm.truncate_tablename(conn, 'wave')
@@ -184,100 +208,113 @@ def wave_analyzer(dm, exchange, symbols, bin_size, bin_time, up_to_count, startp
 
         df_all = df_candle
         df_all.reset_index(inplace=True, drop=False)
-        df_lows = fractals_low_loop(df_all)
-        # df_lows = fractals_low_loop(df_lows)
+        df_lows = fractals_low_loopA(df_all, fractal_count=fractal_count, loop_count=loop_count)
         df_lows_plot = df_lows[['Date', 'Low']]
         wa = WaveAnalyzer(df=df_all, verbose=False)
-        up_to_count = up_to_count if up_to_count else 1
+        up_to_count = up_to_count if up_to_count else 3
         wave_options_impulse = WaveOptionsGenerator5(up_to=up_to_count)
 
         impulse = Impulse('impulse')
         rules_to_check = [impulse]
         wavepattern_up_l = []
         wave_option_plot_l = []
-        wavepatterns_up = set()
+        wavepatterns = set()
+
         mins = df_lows.index.tolist()
+        wavepattern_l = []
+        wave_option_plot_l = []
+
         for i in mins:
             for new_option_impulse in wave_options_impulse.options_sorted:
                 waves_up = wa.find_impulsive_wave(idx_start=i, wave_config=new_option_impulse.values)
                 if waves_up:
-                    wavepattern_up = WavePattern(waves_up, verbose=True)
-                    for rule in rules_to_check:
-                        if wavepattern_up.check_rule(rule):
-                            if wavepattern_up in wavepatterns_up:
-                                continue
-                            else:
-                                wavepatterns_up.add(wavepattern_up)
-                                wavepattern_up_to_list = [rule.name, wavepattern_up.dates, wavepattern_up.values,
-                                                          wavepattern_up.labels]
-                                if not check_has_same_wavepattern_df(df_wave, wavepattern_up):
-                                    w_info = [
-                                              int(time.time() * 1000.0),
-                                              symbol,
-                                              bin_size,
-                                              bin_time,
-                                              rule.name,
-                                              wavepattern_up.dates[0],
-                                              wavepattern_up.dates[9],
-                                              wavepattern_up.low,
-                                              wavepattern_up.values[1],
-                                              wavepattern_up.values[3],
-                                              wavepattern_up.values[5],
-                                              wavepattern_up.values[7],
-                                              wavepattern_up.high,
-                                              'wavedata',
-                                              0,
-                                              0,
-                                              str(datetime.now())
+                    wavepattern = WavePattern(waves_up, verbose=True)
+                    # new condition by neo
+                    if (wavepattern.idx_end - wavepattern.idx_start + 1) >= fractal_count / 2:
+                        for rule in rules_to_check:
+                            if wavepattern.check_rule(rule):
+                                if wavepattern in wavepatterns:
+                                    continue
+                                else:
+                                    wavepatterns.add(wavepattern)
+                                    if not check_has_same_wavepattern(wavepattern_l, wavepattern):
+                                        if dm.check_no_duplicate_wave(symbol, bin_size, bin_time, wavepattern.dates[0], wavepattern.dates[9], conn):
+                                            # new condition by neo
+                                            if not check_has_same_wavepattern_df(df_wave, wavepattern):
+                                                wavepatterns.add(wavepattern)
+                                                wavepattern_l.append([i, wavepattern.dates[0], id(wavepattern), wavepattern])
+                                                w_info = [
+                                                    int(time.time() * 1000.0),
+                                                    symbol,
+                                                    bin_size,
+                                                    bin_time,
+                                                    rule.name,
+                                                    wavepattern.dates[0],
+                                                    wavepattern.dates[9],
+                                                    wavepattern.values[0],
+                                                    wavepattern.values[1],
+                                                    wavepattern.values[3],
+                                                    wavepattern.values[5],
+                                                    wavepattern.values[7],
+                                                    wavepattern.values[9],
+                                                    'wavedata',
+                                                    0,
+                                                    0,
+                                                    str(datetime.now())
 
-                                    ]
-                                    if dm.check_no_duplicate_wave(symbol, bin_size, bin_time, wavepattern_up.dates[0], wavepattern_up.dates[9], conn):
-                                        dm.save_wave(w_info, conn)
-                                        wavepattern_up_l.append(wavepattern_up)
-                                        wave_option_plot_l.append([
-                                            [str(wavepattern_up.waves['wave5'].dates[1])],
-                                            [wavepattern_up.waves['wave5'].high],
-                                            [str(new_option_impulse.values)]
-                                        ])
-                                        print('check_no_duplicate_wave:%s' % str(w_info))
+                                                ]
 
-        # if plotview:
-        #     if wavepattern_up_l:
-        #         t = str(symbol + '_ALL_' + str(bin_size) + '_' + str(i) + ':' + rule.name + ' ' + str(new_option_impulse.values) + ' ')
-        #         plot_pattern_m(df=df_all, wave_pattern=wavepattern_up_l,
-        #                        df_plot=df_lows_plot,
-        #                        wave_options=wave_option_plot_l, title=t)
-        #         print(t)
-    conn.commit()
-    conn.close()
+                                                dm.save_wave(w_info, conn)
+                                                wavepattern_up_l.append(wavepattern)
+                                                wave_option_plot_l.append([
+                                                    [str(wavepattern.waves['wave5'].dates[1])],
+                                                    [wavepattern.waves['wave5'].high],
+                                                    [str(new_option_impulse)]
+                                                ])
+                                                # print(f'{rule.name} found: {new_option_impulse.values}')
+                                                print('%s %s:save_wave:%s ' % (symbol, str(new_option_impulse.values), str(w_info)))
+
+        if plotview:
+            if wavepattern_up_l:
+                t = str(symbol + '_fractal%s_'% str(fractal_count) + str(bin_size) + 'm_' + rule.name + ' ')
+                plot_pattern_m(df=df_all, wave_pattern=wavepattern_up_l,
+                               df_plot=df_lows_plot,
+                               wave_options=wave_option_plot_l, title=t)
+                # print(t)
+
+    if conn:
+        conn.commit()
+        conn.close()
 
 
 ##################################################
 #### 1 (wave_analyzer scheduler)
 ##################################################
+pairs = Bybit.symbols()
+find_USDT = 'USDT-PERP'
+index_USDTs = [i for i in range(len(pairs)) if find_USDT in pairs[i]]
+
+symbols_usdt = []
+for s in index_USDTs:
+    symbols_usdt.append(pairs[s])
+symbols = symbols_usdt
+
+
 async def ohlcv(data):
     start = time.perf_counter()
     try:
-        from datapostgre import DataManagerfeeder
-    except:
-        from .datapostgre import DataManagerfeeder
-    pairs = Bybit.symbols()
-    find_USDT = 'USDT-PERP'
-    index_USDTs = [i for i in range(len(pairs)) if find_USDT in pairs[i]]
-
-    symbols_usdt = []
-    for s in index_USDTs:
-        symbols_usdt.append(pairs[s])
-    symbols = symbols_usdt
-    dm = DataManagerfeeder()
-    window = 1440 / 2
-    nowpoint = datetime.now()
-    truncate = False
-    bin_size = 1
-    bin_time = 'm'
-    wave_analyzer(dm, exchange, symbols, bin_size, bin_time, up_to_count=up_to_count,
-                                           startpoint=None, nowpoint=nowpoint, window=window, truncate=truncate)
-    print(f'Finished wave_analyzer in {round(time.perf_counter() - start, 2)} second(s)')
+        # window = 1440 / 2
+        window = fractal_count * 10 if fractal_count * 10 > 100 else 100
+        nowpoint = datetime.now()
+        truncate = False
+        bin_size = 1
+        bin_time = 'm'
+        wave_analyzer(exchange, symbols, bin_size, bin_time, up_to_count=up_to_count,
+                                               startpoint=None, nowpoint=nowpoint, window=window, truncate=truncate)
+        print(f'Finished wave_analyzer in {round(time.perf_counter() - start, 2)} second(s)')
+    except Exception as e:
+        print('ohlcv:Exception e: %s' % e)
+        pass
 
 
 def main():
@@ -289,4 +326,6 @@ def main():
 
 
 if __name__ == '__main__':
+    pid = os.getpid()
+    print('wave_analyzer start pid: %s' % pid)
     main()
